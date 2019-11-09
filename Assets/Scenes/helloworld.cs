09e6c0fb7b98fb9fb833b3f55b5c4dd41e3b5769
Hypnos507 <49710247+Hypnos507@users.noreply.github.com>
2019-11-09 19:08:01 +0800

    Add files via upload

---
+++ Assets/Scenes/helloworld.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class helloworld : MonoBehaviour
+{
+    public float speed = 1;
+    public Transform obj;
+    private float lastTime;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Right
+        if (Input.GetKeyDown(KeyCode.RightArrow)){
+
+                lastTime = Time.time;
+
+        }       
+
+        if (Input.GetKey(KeyCode.RightArrow)) {
+
+             
+              obj.transform.position += new Vector3((Time.time - lastTime)*speed, 0, 0);
+              lastTime = Time.time;
+        }
+
+        // Left
+        if (Input.GetKeyDown(KeyCode.LeftArrow)){
+
+                lastTime = Time.time;
+
+        }       
+
+        if (Input.GetKey(KeyCode.LeftArrow)) {
+
+             
+              obj.transform.position -= new Vector3((Time.time - lastTime)*speed, 0, 0);
+              lastTime = Time.time;
+        }
+    }
+}
+
+
