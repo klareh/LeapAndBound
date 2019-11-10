5b8dfe5ac4da8cbeb1b313773fa85597478cd3af
klareH <klare0216@gmail.com>
2019-11-10 12:45:58 +0800

    Added: 玩家空白建射出八個方向的子彈

---
+++ Assets/Attach.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attach : MonoBehaviour
+{
+
+    public Bullet bullet;
+    public Transform player;
+
+
+    public float speed = 3;
+    public float bulletXSpeed = 0;
+    public float bulletYSpeed = 0;
+    public Vector3 bulletVector = new Vector3(0, 0, 0);
+
+
+    void Start()
+    {
+        bulletYSpeed = speed;
+    }
+
+    void Update()
+    {
+        updateFaceDirection();
+
+        if (Input.GetKey(KeyCode.Space)) {
+            Vector3 initPosition = getPlayerPosition();
+            Bullet p = Instantiate(bullet, initPosition, Quaternion.identity);
+            p.velocity = new Vector3(bulletXSpeed, bulletYSpeed, 0);
+        }
+    }
+
+    private void updateFaceDirection() {
+        if (Input.GetKey(KeyCode.RightArrow)) 
+            setBulletSpeed(speed, 0);
+
+        if (Input.GetKey(KeyCode.LeftArrow)) 
+            setBulletSpeed(-speed, 0);
+
+        if (Input.GetKey(KeyCode.UpArrow)) 
+            setBulletSpeed(0, speed);
+        
+        if (Input.GetKey(KeyCode.DownArrow)) 
+            setBulletSpeed(0, -speed);
+
+        if (Input.GetKey(KeyCode.RightArrow) && Input.GetKey(KeyCode.UpArrow)) 
+            setBulletSpeed(speed, speed);
+
+        if (Input.GetKey(KeyCode.RightArrow) && Input.GetKey(KeyCode.DownArrow)) 
+            setBulletSpeed(speed, -speed);
+
+        if (Input.GetKey(KeyCode.LeftArrow) && Input.GetKey(KeyCode.UpArrow)) 
+            setBulletSpeed(-speed, speed);
+
+        if (Input.GetKey(KeyCode.LeftArrow) && Input.GetKey(KeyCode.DownArrow)) 
+            setBulletSpeed(-speed, -speed);
+
+    }
+
+
+    private void setBulletSpeed(float x, float y) {
+        bulletXSpeed = x;
+        bulletYSpeed = y;
+    }
+
+    private Vector3 getPlayerPosition() {
+        return player.transform.position;
+    }
+}
---
+++ Assets/Bullet.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    private Rigidbody2D rigidBody;
+    public Vector3 velocity = new Vector3(0, 0, 0);
+
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+
+    }
+
+    void Update()
+    {
+        rigidBody.velocity = velocity;
+    }
+
+    private void OnCollisionEnter2D(Collision2D other) {
+        Destroy(gameObject);
+    }
+
+}
