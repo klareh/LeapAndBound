c661026d58d5a90a719ca5096a9a3bdec4c5b02a
klareH <klare0216@gmail.com>
2019-11-12 23:45:34 +0800

    Added: 玩家碰到出口回到初入地點

---
+++ Assets/Scripts/Exit.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Exit : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    private void OnTriggerEnter2D(Collider2D other) 
+    {
+        if (other.gameObject.name == "player")      
+            StartCoroutine(nextFloor(other.gameObject, 0.5f));
+    }
+
+    IEnumerator nextFloor(GameObject gameObject, float delay)
+    {
+        Transform transform = gameObject.GetComponent(typeof(Transform)) as Transform;
+        yield return new WaitForSeconds(delay);
+        transform.position = new Vector3(0, 0, -20);
+        yield return new WaitForSeconds(delay);
+        transform.position = new Vector3(0, 0, -6);
+    }
+
+}
